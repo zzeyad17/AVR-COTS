dcdfac4a8b6e3732244d89f83b06cb8e1661882f
zzeyad17 <32441781+zzeyad17@users.noreply.github.com>
2021-08-03 12:10:47 +0200

    Add files via upload

---
+++ LIB/Bit_Math.h
@@ -0,0 +1,17 @@
+/********************************************************************************/
+/*  Author        :  Zeyad Mohsen Mohamed                                       */
+/*  Date          :	 1 DEC 2020                                                 */
+/*  Version       :  V01                                                        */
+/********************************************************************************/
+
+#ifndef _BIT_MATH_H
+#define _BIT_MATH_H
+
+//********** Bits Manipulation Macros ***************************//
+
+#define SET_BIT(reg,bitN)    ((reg) |= (1<<bitN))    // Sets a bit (bitN) in a register (reg)
+#define CLR_BIT(reg,bitN)  ((reg) &= ~(1<<bitN))     // Clears a bit (bitN) in a register (reg)
+#define TOG_BIT(reg,bitN) ((reg) ^= (1<<bitN))       // Toggles a bit (bitN) in a register (reg)
+#define GET_BIT(reg,bitN)    (1 & ((reg)>>bitN))     // Returns value of bit (bitN) in a register (reg)
+			
+#endif
